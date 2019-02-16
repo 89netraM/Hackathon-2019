10701fe42d6f394ef31d3c2332146e8c7864f023
Joakim <joakimanderlind@gmail.com>
2019-02-16 15:55:11 +0100

    Popup2

--- app/src/main/java/com/winnerisolle/apa/MainActivity.java
+++ app/src/main/java/com/winnerisolle/apa/MainActivity.java
@@ -1,9 +1,12 @@
 package com.winnerisolle.apa;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
@@ -39,21 +42,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void debugPopUp(View view){
-        LayoutInflater li = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View v = li.inflate(R.layout.report,null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
 
-        final PopupWindow pw = new PopupWindow(getApplicationContext());
-        pw.setContentView(v);
-
-        Button sub = (Button) v.findViewById(R.id.submit);
-        sub.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                pw.dismiss();
-            }
-        });
-
+        builder.setView(inflater.inflate(R.layout.report, null));
+        builder.create().show();
     }
 
 }
